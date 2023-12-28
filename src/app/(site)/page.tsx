9e424d69d3f51a3c598318e907553d58f19cc240
purupurupu <purupurupu0prog@gmail.com>
2023-12-28 22:23:48 +0900

    Add new images and icons. Add logo animation.

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,24 +3,26 @@ import TitleSection from "@/components/landing-page/title-section";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Banner from "../../../public/appBanner.png";
+import { CLIENTS } from "@/lib/constants";
 
 function HomaPage() {
   return (
-    <section className="gap-4 px-4 mt-10 overflow-hidden sm:px-6 sm:flex sm:flex-col md:justify-center md:items-center">
-      <TitleSection
-        pill="Your Workspace, Perfected"
-        title="All-In-One Collaboration and Productivity Platform"
-      />
-      <div className="bg-white p-[2px] mt-6 rounded-xl bg-gradient-to- from-primary to-brand-primaryBlue sm:w-[300px]">
-        <Button
-          variant="secondary"
-          className="w-full rounded-[10px] p-6 text-2xl bg-background"
-        >
-          Get Free
-        </Button>
-      </div>
-      <div
-        className="md:mt-[-90px]
+    <>
+      <section className="gap-4 px-4 mt-10 overflow-hidden sm:px-6 sm:flex sm:flex-col md:justify-center md:items-center">
+        <TitleSection
+          pill="Your Workspace, Perfected"
+          title="All-In-One Collaboration and Productivity Platform"
+        />
+        <div className="bg-white p-[2px] mt-6 rounded-xl bg-gradient-to- from-primary to-brand-primaryBlue sm:w-[300px]">
+          <Button
+            variant="secondary"
+            className="w-full rounded-[10px] p-6 text-2xl bg-background"
+          >
+            Get Free
+          </Button>
+        </div>
+        <div
+          className="md:mt-[-90px]
           sm:w-full
           w-[750px]
           flex
@@ -30,10 +32,10 @@ function HomaPage() {
           relative
           sm:ml-0
           ml-[-50px]"
-      >
-        <Image src={Banner} alt="application banner" />
-        <div
-          className="bottom-0
+        >
+          <Image src={Banner} alt="application banner" />
+          <div
+            className="bottom-0
             top-[50%]
             bg-gradient-to-t
             dark:from-background
@@ -42,9 +44,64 @@ function HomaPage() {
             absolute
             z-10
           "
-        ></div>
-      </div>
-    </section>
+          ></div>
+        </div>
+      </section>
+      <section className="relative">
+        <div
+          className="overflow-hidden
+          flex
+          after:content['']
+          after:dark:from-brand-dark
+          after:to-transparent
+          after:from-background
+          after:bg-gradient-to-l
+          after:right-0
+          after:bottom-0
+          after:top-0
+          after:w-20
+          after:z-10
+          after:absolute
+
+          before:content['']
+          before:dark:from-brand-dark
+          before:to-transparent
+          before:from-background
+          before:bg-gradient-to-r
+          before:left-0
+          before:top-0
+          before:bottom-0
+          before:w-20
+          before:z-10
+          before:absolute
+        "
+        >
+          {[...Array(2)].map((arr) => (
+            <div key={arr} className="flex flex-nowrap animate-slide">
+              {CLIENTS.map((client) => (
+                <div
+                  key={client.alt}
+                  className="relative
+                w-[200ox]
+                m-20
+                shrink-0
+                flex
+                items-center
+                "
+                >
+                  <Image
+                    src={client.logo}
+                    alt={client.alt}
+                    width={200}
+                    className="object-contain max-w-none"
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
 
