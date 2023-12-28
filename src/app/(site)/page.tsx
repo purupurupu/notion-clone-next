be68f01f3b09555acf2f0296dac7d1fadcce5cce
purupurupu <purupurupu0prog@gmail.com>
2023-12-28 21:59:26 +0900

    Add app banner image and update homepage layout

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,23 +1,48 @@
 import React from "react";
 import TitleSection from "@/components/landing-page/title-section";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Banner from "../../../public/appBanner.png";
 
 function HomaPage() {
   return (
     <section className="gap-4 px-4 mt-10 overflow-hidden sm:px-6 sm:flex sm:flex-col md:justify-center md:items-center">
-      <div>
-        <TitleSection
-          pill="Your Workspace, Perfected"
-          title="dfdsafdsafdsafsd"
-        />
-        <div className="bg-white p-[2px] mt-[6] rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]">
-          <Button
-            variant="secondary"
-            className="w-full rounded-[10px] p-6 text-2xl bg-background"
-          >
-            fdsafsa
-          </Button>
-        </div>
+      <TitleSection
+        pill="Your Workspace, Perfected"
+        title="All-In-One Collaboration and Productivity Platform"
+      />
+      <div className="bg-white p-[2px] mt-6 rounded-xl bg-gradient-to- from-primary to-brand-primaryBlue sm:w-[300px]">
+        <Button
+          variant="secondary"
+          className="w-full rounded-[10px] p-6 text-2xl bg-background"
+        >
+          Get Free
+        </Button>
+      </div>
+      <div
+        className="md:mt-[-90px]
+          sm:w-full
+          w-[750px]
+          flex
+          justify-center
+          items-center
+          mt-[-40px]
+          relative
+          sm:ml-0
+          ml-[-50px]"
+      >
+        <Image src={Banner} alt="application banner" />
+        <div
+          className="bottom-0
+            top-[50%]
+            bg-gradient-to-t
+            dark:from-background
+            left-0
+            right-0
+            absolute
+            z-10
+          "
+        ></div>
       </div>
     </section>
   );
