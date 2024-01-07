e72af604b9ebbb2e6e731811796a481b4a0f1311
purupurupu <purupurupu0prog@gmail.com>
2024-01-07 23:52:48 +0900

    recreate table

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,7 +1,30 @@
+import db from "@/lib/supabase/db";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const Page = () => {
-  return <div>Page</div>;
+const DashboardPage = async () => {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) return;
+
+  const workspace = await db.query.workspaces.findFirst({
+    where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
+  });
+
+  if (!workspace)
+    return (
+      <div className="flex items-center justify-center w-screen h-screen bg-background">
+        {/* <DashboardSetup>Workspace not found</DashboardSetup> */}
+      </div>
+    );
+
+  redirect(`/dashboard/${workspace.id}`);
 };
 
-export default Page;
+export default DashboardPage;
