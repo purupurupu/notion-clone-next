1de506553a9d71e5af4cf7e71514af172da1313d
purupurupu <purupurupu0prog@gmail.com>
2024-01-15 22:38:12 +0900

    Add icons and links for settings and trash in sidebar

--- src/components/sidebar/native-navigation.tsx
+++ src/components/sidebar/native-navigation.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 import { twMerge } from "tailwind-merge";
 import CypressHomeIcon from "../icons/cypressHomeIcon";
+import CypressSettingsIcon from "../icons/cypressSettingsIcon";
+import CypressTrashIcon from "../icons/cypressTrashIcon";
 
 interface NativeNavigationProps {
   myWorkspaceId: string;
@@ -14,7 +16,7 @@ const NativeNavigation: React.FC<NativeNavigationProps> = ({
 }) => {
   return (
     <nav className={twMerge("my-2", className)}>
-      <ul className="flex flex-col">
+      <ul className="flex flex-col gap-2">
         <li>
           <Link
             className="flex gap-2 transition-all group/native text-Neutrals/neutrals-7"
@@ -22,6 +24,24 @@ const NativeNavigation: React.FC<NativeNavigationProps> = ({
           >
             <CypressHomeIcon />
             <span>My Workspace</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="flex gap-2 transition-all group/native text-Neutrals/neutrals-7"
+            href={`/dashboard/${myWorkspaceId}`}
+          >
+            <CypressSettingsIcon />
+            <span>Settings</span>
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="flex gap-2 transition-all group/native text-Neutrals/neutrals-7"
+            href={`/dashboard/${myWorkspaceId}`}
+          >
+            <CypressTrashIcon />
+            <span>Trash</span>
           </Link>
         </li>
       </ul>
